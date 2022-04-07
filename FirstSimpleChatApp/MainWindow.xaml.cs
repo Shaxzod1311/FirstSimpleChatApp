6e34c057bd8280eb4cddc39542d0c7bbe2a1b110
Shaxzod1311 <97043620+Shaxzod1311@users.noreply.github.com>
2022-04-08 00:53:20 +0500

    Add project files.

---
+++ FirstSimpleChatApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstSimpleChatApp
+{
+
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HubConnection hubConnection;
+        private Message message;
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Action<Message> action1 = (message) =>
+            {
+                listview.Items.Add($"{message.Text}: {message.Date}");
+            };
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://signal-r-app.herokuapp.com/chat").Build();
+            message = new Message();
+
+            hubConnection.On<Message>("Send", action1.Invoke);
+
+            Action action = async () =>
+            {
+                await hubConnection.StartAsync();
+            };
+
+            action.Invoke();
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            message.Text = textbox.Text;
+            message.Date = DateTime.Now.ToString();
+            listview.Items.Add(message.Text);
+
+            Action action = async () =>
+            {
+                await hubConnection.SendAsync("SendMessage", message);
+            };
+
+            action.Invoke();
+        }
+             
+    }   
+}
---
+++ FirstSimpleChatApp/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FirstSimpleChatApp
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public string Date { get; set; }
+    }
+}
